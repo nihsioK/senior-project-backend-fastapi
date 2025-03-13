82563e681a48dac85c64ace16d6eddefb5754c37
Daniyar Koishin <dandevko@gmail.com>
2025-03-13 21:42:13 +0500

    fix

--- app/config.py
+++ app/config.py
@@ -8,5 +8,5 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/senior-project-2")
-DATABASE_URL = "postgresql://dandevko:dandevko@localhost/senior_project"
+DATABASE_URL = "postgresql://dandevko:dandevko@localhost/senior_project_2"
 # DATABASE_URL = "postgresql://postgres:postgres@localhost/senior-project-2"
